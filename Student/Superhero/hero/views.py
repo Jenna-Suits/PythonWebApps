0e35eb66bf82d9d4757028cf57edfc32f187bcc1
Jenna-Suits <157147567+Jenna-Suits@users.noreply.github.com>
2024-09-12 18:53:06 -0600

    update

--- Student/Superhero/hero/views.py
+++ Student/Superhero/hero/views.py
@@ -6,5 +6,14 @@ class HulkView(TemplateView):
         return {
             'title': 'Hulk',
             'id': 'Bruce Banner',
-            'image': '/static/images/hulk.jpg'
+            'image': 'Student\Superhero\static\images\hulk.jpg'
+        }
+
+class IronManView(TemplateView):
+    template_name = 'hero.html'
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'IronMan',
+            'id': 'Tony Stark',
+            'image': 'Superhero/static/images/iron_man.jpg'
         }
