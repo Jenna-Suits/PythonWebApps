8fb4fc6c416d217230212485ff235ea778c1e0db
Jenna-Suits <157147567+Jenna-Suits@users.noreply.github.com>
2024-09-12 18:39:32 -0600

    update

--- Student/Superhero/hero/views.py
+++ Student/Superhero/hero/views.py
@@ -2,3 +2,9 @@ from django.views.generic import TemplateView
 
 class HulkView(TemplateView):
     template_name = 'hero.html'
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'Hulk',
+            'id': 'Bruce Banner',
+            'image': '/static/images/hulk.jpg'
+        }
