45e28014ae5f05f869f7919caef39f41a83f8117
Jenna-Suits <157147567+Jenna-Suits@users.noreply.github.com>
2024-10-04 14:08:22 -0600

    Update models.py

--- ClassroomDemos/NotePad/note/models.py
+++ ClassroomDemos/NotePad/note/models.py
@@ -1,36 +1,10 @@
-# Add a new record
-Note.objects.create(title="My Note", author="Me", body="NONE")
+from django.db import models
 
-# List all objects
-Note.objects.all()
 
-# List specific objects
-Note.objects.filter(author='Me')
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    body = models.TextField()
 
-# Get object by Primary Key
-n = Note.objects.get(pk="4")
-
-# Get object by Title
-n = Note.objects.get(title="My Note")
-
-# Show the details
-print(n.pk, n.title, n.author)
-
-# Get object by Title
-n = Note.objects.get(title="My Note")
-
-# Modify a field
-n.body = "New text to show"
-n.save()
-
-# Delete one object
-n = Note.objects.get(pk="16")
-n.delete()
-
-# Delete multiple objects
-n = Note.objects.filter(author="Me").delete()
-
-def list_notes():
-    for n in Note.objects.all():
-        print(n.pk, n.title, n.author)
-
+    def __str__(self):
+        return f'{self.pk}. {self.title} - {self.author}'
