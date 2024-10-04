0660b60876d2c5dfe268adcfa01c4a6c16c88424
Jenna-Suits <157147567+Jenna-Suits@users.noreply.github.com>
2024-10-04 14:14:58 -0600

    project 5

--- ClassroomDemos/Superhero/hero/models.py
+++ ClassroomDemos/Superhero/hero/models.py
@@ -3,12 +3,12 @@ from django.urls import reverse_lazy
 
 
 class Superhero(models.Model):
-    name = models.CharField(max_length=200)
-    identity = models.CharField(max_length=200)
-    description = models.TextField(default="None")
-    image = models.CharField(max_length=200, default="None")
-    strengths = models.CharField(max_length=200, default="None")
-    weaknesses = models.CharField(max_length=200, default="None")
+    name = models.CharField(max_length=100)
+    identity = models.CharField(max_length=100)
+    description = models.TextField()
+    strength = models.CharField(max_length=100)
+    weakness = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
 
     def __str__(self):
         return self.identity
