6da9fef226e74cf5e212e0375cf7fa76d35ef62d
Jenna-Suits <157147567+Jenna-Suits@users.noreply.github.com>
2024-11-11 00:26:58 -0700

    Project 7

--- Student/hero/models.py
+++ Student/hero/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Superhero(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
+    identity = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.CharField(max_length=200)
+    strength = models.CharField(max_length=100)
+    weakness = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
